dea489a0446f42ee5e067ce069c42fae88d402b1
Christian Helle <christian.helle@outlook.com>
2026-02-19 22:30:20 +0100

    feat: update main to use cli module

--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,23 @@
 const std = @import("std");
-const walker = @import("walker.zig");
-const results_mod = @import("results.zig");
+const cli_mod = @import("cli.zig");
 const progress_mod = @import("progress.zig");
-
-const VERSION = "0.1.0";
-const HELP =
-    \\Usage: clocz [options] [path]
-    \\
-    \\Count lines of code in a directory tree.
-    \\
-    \\Arguments:
-    \\  path          Directory to scan (default: current directory)
-    \\
-    \\Options:
-    \\  -h, --help    Print this help and exit
-    \\  -v, --version Print version and exit
-    \\
-;
+const results_mod = @import("results.zig");
+const walker = @import("walker.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    const cli = try cli_mod.Cli.init(allocator);
+    defer cli.deinit();
 
-    var scan_path: []const u8 = ".";
-
-    for (args[1..]) |arg| {
-        if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
-            try std.fs.File.stdout().writeAll(HELP);
-            return;
-        } else if (std.mem.eql(u8, arg, "-v") or std.mem.eql(u8, arg, "--version")) {
-            var buf: [32]u8 = undefined;
-            var fw = std.fs.File.stdout().writer(&buf);
-            try fw.interface.print("clocz {s}\n", .{VERSION});
-            try fw.interface.flush();
-            return;
-        } else if (arg.len > 0 and arg[0] != '-') {
-            scan_path = arg;
-        } else {
-            var buf: [256]u8 = undefined;
-            const msg = try std.fmt.bufPrint(&buf, "Unknown option: {s}\n\n{s}", .{ arg, HELP });
-            try std.fs.File.stderr().writeAll(msg);
-            std.process.exit(1);
-        }
+    if (cli.options.help) {
+        try cli_mod.show_help();
+        return;
+    } else if (cli.options.version) {
+        try cli_mod.show_version();
+        return;
     }
 
     var results = results_mod.Results.init(allocator);
@@ -59,10 +31,10 @@ pub fn main() !void {
 
     var timer = try std.time.Timer.start();
 
-    walker.walk(allocator, scan_path, &results, &pool) catch |err| {
+    walker.walk(allocator, cli.path, &results, &pool) catch |err| {
         progress.stop(progress_thread);
         var buf: [256]u8 = undefined;
-        const msg = try std.fmt.bufPrint(&buf, "error: cannot open '{s}': {s}\n", .{ scan_path, @errorName(err) });
+        const msg = try std.fmt.bufPrint(&buf, "error: cannot open '{s}': {s}\n", .{ cli.path, @errorName(err) });
         std.fs.File.stderr().writeAll(msg) catch {};
         pool.deinit();
         std.process.exit(1);
